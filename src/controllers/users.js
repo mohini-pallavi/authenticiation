0016c8d4b04162daefdc0829064c3dd0dbe0056f
PALLAVI MOHINI <1804395@kiit.ac.in>
2020-11-09 23:33:48 +0530

    edit

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -7,20 +7,39 @@ const dbFilePath = `${__dirname}../../../data/user-db.json`;
 module.exports = {
     authenticate: async (req, res, next) => {
         const adminData = await readFile(dbFilePath);
-        const email=req.user.email;
-        const user=adminData.find(
-            (user) => user.email === email
-        );
-        if(user.email && user.password)
-        {
-            const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '0.2h' });
-            res.json({ status: 200, data: { token } });
+        //Note : YOUR CODE:
 
-        }
-        else{
-            next({ status: 400, message: "Invalid username/password !", data: null });
+        // const email=req.user.email;
+        // const user=adminData.find(
+        //     (user) => user.email === email
+        // );
+        // if(user.email && user.password)
+        // {
+        //     const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '0.2h' });
+        //     res.json({ status: 200, data: { token } });
+        //
+        // }
+        // else{
+        //     next({ status: 400, message: "Invalid username/password !", data: null });
+        //
+        // }
 
-        }
+        const username = req.body.username;
+       const password = req.body.password;
+
+       const oneUser = adminData.filter(function (user){
+           if(user.username === username && user.password === password){
+               return user;
+           }
+       });
+
+       if(oneUser) {
+           const token = jwt.sign({ id: oneUser.id }, req.app.get('secretKey'), { expiresIn: '1h' });
+           res.json({ status: 200, data: { token } });
+       }
+        else {
+           next({ status: 400, message: "Invalid username/password !", data: null });
+       }
       //NOTE: match if username & password are equal to the ones from user-db.json
         /** if true:
         * const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '1h' });
