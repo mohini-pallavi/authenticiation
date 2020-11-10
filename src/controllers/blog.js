45d45960c133ca72149d845b29ea17dcc3f5403c
PALLAVI MOHINI <1804395@kiit.ac.in>
2020-11-11 01:11:51 +0530

    edit

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,5 +1,5 @@
 const { readFile, writeFile } = require('../daos/index'); //NOTE: Importing the readFile & writeFile as separate functions
-
+const dbFilePath = `${__dirname}../../../data/blog-db.json`;
 module.exports = {
     getById: async (req, res, next) => {
         try {
@@ -41,6 +41,7 @@ module.exports = {
         //NOTE: implement create operation same way as course day 7
     },
     updateById :  async (req, res, next) => {
+        const blogs = await readFile(dbFilePath);
         const blogId = Number(request.params.id);
   const body = request.body;
   const blog = blogs.find((blog) => blog.id === blogId);
@@ -55,6 +56,32 @@ module.exports = {
 
     response.status(200).send(updatedBlog);
   }
+//   try{
+//             const id = Number(req.params.id);
+//             const blogs = await readFile(dbFilePath);
+//             const newBlog = req.body;
+
+//             let flag = 0;
+//             let index = -1;
+
+//             for(let i in blogs){
+//                 if(blogs[i].id === id){
+//                     blogs[i] = {...blogs[i],...newBlog};
+//                     index = i;
+//                     flag = 1;
+//                     break;
+//                 }
+//             }
+
+//             if(flag === 1){
+//                 await writeFile(dbFilePath,blogs);
+//                 res.send(blogs[index]);
+//             }else{
+//                 res.status(404).send(`Account with id: ${id} not found!`);
+//             }
+//         }catch(error){
+//             next(error);
+//         }
 },
 
         //NOTE: implement update operation same way as course day 7
@@ -63,7 +90,7 @@ module.exports = {
     deleteById: async (req, res, next) => {
         
             const blogId = Number(request.params.id);
-            const blognew = blogs.filter((blogs) => blogs.id != accountId);
+            const blognew = blogs.filter((blogs) => blogs.id != blogId);
           
             if (!blognew) {
               response.status(404).send("blogs not found.");
@@ -73,5 +100,28 @@ module.exports = {
             }
           
         //NOTE: implement delete operation same way as course day 7
+        // try{
+        //     const id = Number(req.params.id);
+        //     const blogs = await readFile(dbFilePath);
+
+        //     let flag = 0;
+
+        //     blogs = blogs.filter(function(blog){
+        //         if(blog.id === id){
+        //             flag = 1;
+        //             return blog.id !== id;
+        //         } 
+        //     }) 
+
+
+        //     if(flag === 1){
+        //         await writeFile(dbFilePath,blogs);
+        //         res.send("Deleted successfully");
+        //     }else{
+        //         res.status(404).send(`Account with id: ${id} not found!`);
+        //     }
+        // }catch(error){
+        //     next(error);
+        // }
     }
 }
